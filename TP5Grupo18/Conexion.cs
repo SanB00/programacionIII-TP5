4eaa55689d729cd90c29dee68888b978231dc5be
SantiB <151892199+SanB00@users.noreply.github.com>
2026-05-11 19:14:12 -0300

    de Lautaro Nehuen Gimenez - domingo, 10 de mayo de 2026, 18:39 Buenas, hice la clase Conexion como se pide en la consigna.
    Desde allí agreguen sus rutas de conexión y van comentando las que no usen, tambien deje la que pide el pdf para entrega.
    
    La funcion dentro de la clase es como la hizo el profe en el video, devuelve un int y de parametro hay que poner la consulta que vamos a usar.
    
     TP5Grupo18-v1.1.zip

---
+++ TP5Grupo18/Conexion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace TP5Grupo18
+{
+    public class Conexion
+    {
+        //cadenaParaEntrega
+        
+        //private const string cadenaConexion = @"Data Source=localhost\\sqlexpress; Initial Catalog=BDSucursales;Integrated Security=True";
+        //franco
+        private const string cadenaConexion = @"localhost\sqlexpress;Initial Catalog=BDSucursales;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        //Lautaro
+        //private const string cadenaConexion = @"Data Source=localhost;Initial Catalog=BDSucursales;Integrated Security=True";
+
+
+        public int ejecutarTransaccion(string consultaSQL)
+        {
+            SqlConnection sqlConnection = new SqlConnection(cadenaConexion);
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand(consultaSQL, sqlConnection);
+
+            int filasAfectadas = sqlCommand.ExecuteNonQuery(); /// INSERT, UPDATE, DELETE
+
+            sqlConnection.Close();
+
+            return filasAfectadas;
+
+        }
+    }
+}
