0f92cb53237929765f3fe429fe1708268a1dd8c9
SantiB <151892199+SanB00@users.noreply.github.com>
2026-05-12 12:04:31 -0300

    de Elian Gaston Maspero - martes, 12 de mayo de 2026, 01:32 Continuando con el tp, decidí agregar la función que utilizaremos posteriormente para limpiar los campos
     TP5Grupo18-v1.3.rar

--- TP5Grupo18/AgregarSucursal.aspx.cs
+++ TP5Grupo18/AgregarSucursal.aspx.cs
@@ -4,14 +4,21 @@ namespace TP5Grupo18
 {
     public partial class AgregarSucursal : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        protected void Page_Load(object sender, EventArgs e) {
 
         }
 
-        protected void btnAceptar_Click(object sender, EventArgs e)
-        {
+        protected void btnAceptar_Click(object sender, EventArgs e) {
 
+        }
+
+        private void LimpiarControles() {
+            txtNombre.Text = "";
+            txtDescripcion.Text = "";
+            txtDireccion.Text = "";
+            if (ddlProvincias.Items.Count > 0) {
+                ddlProvincias.SelectedIndex = 0;
+            }
         }
     }
 }
