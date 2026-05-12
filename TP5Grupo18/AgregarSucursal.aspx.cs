d9fb365d5278328ffc07d57217b6d28d7a45552a
SantiB <151892199+SanB00@users.noreply.github.com>
2026-05-12 12:07:48 -0300

    de FRANCO TOMAS CASAMENTO - martes, 12 de mayo de 2026, 11:11 Buenas, bueno ahí saque la conexión de mas y hice un método para poder hacer consultas, llamado ejecutarConsultas, y en agregar sucursal agregue el metodo cargarProvincia, para poder rellenar los items del ddl desde la base de datos

--- TP5Grupo18/AgregarSucursal.aspx.cs
+++ TP5Grupo18/AgregarSucursal.aspx.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
 
 namespace TP5Grupo18
 {
     public partial class AgregarSucursal : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e) {
+            if (!IsPostBack) {
+                cargarProvincias();
+            }
+        }
+        private void cargarProvincias() {
+            const string consultaSQL = "SELECT Id_Provincia, DescripcionProvincia FROM Provincia ORDER BY DescripcionProvincia";
+            DataTable dataTable = new ConexionBBDD().obtenerTablaDeLaBaseDeDatos("SELECT * FROM Provincia");
 
+            ddlProvincias.DataSource = dataReader;
+            ddlProvincias.DataTextField = "DescripcionProvincia";
+            ddlProvincias.DataValueField = "Id_Provincia";
+            ddlProvincias.DataBind();
+            ddlProvincias.Items.Insert(0, new ListItem("-- Seleccione --", "0"));
         }
 
         protected void btnAceptar_Click(object sender, EventArgs e) {
